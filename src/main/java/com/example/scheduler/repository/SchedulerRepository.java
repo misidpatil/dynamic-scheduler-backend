e4db6aea7af8eaec57247f6bbc83c7e39cf582dc
misidpatil <siddharthp002@gmail.com>
2024-11-26 11:26:31 +0530

    Added missing annotations

--- src/main/java/com/example/scheduler/repository/SchedulerRepository.java
+++ src/main/java/com/example/scheduler/repository/SchedulerRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 public interface SchedulerRepository extends JpaRepository<SchedulerEntity, Long> {
 
     @Modifying
+    @Transactional
     @Query("UPDATE com.example.scheduler.entity.SchedulerEntity s SET s.lastTriggeredTime = :triggeredTime, s.lastCompletionTime = :completionTime WHERE s.id = :id")
     void updateLastTriggerAndCompletionTime(Long id, LocalDateTime triggeredTime, LocalDateTime completionTime);
 }
